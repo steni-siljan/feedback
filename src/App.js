d187695e37d09bc6ad83fbafd627db1360315ea6
steni_frgr <steni@qburst.com>
2024-01-19 12:41:06 +0530

    Added initial files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginForm from './Components/LoginForm';
+
+function App() {
+  return (
+    <div>
+      <LoginForm />
+   
+    </div>
+  );
+}
+
+export default App;
